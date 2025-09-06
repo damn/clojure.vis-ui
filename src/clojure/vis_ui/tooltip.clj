01ed1b3b70333706a628077beefa4a4c7016da74
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 16:37:37 +0530

    Fix update-fn

--- src/clojure/vis_ui/tooltip.clj
+++ src/clojure/vis_ui/tooltip.clj
@@ -5,28 +5,30 @@
             [clojure.vis-ui.label :as label])
   (:import (com.kotcrab.vis.ui.widget Tooltip)))
 
-(defn- update-fn [tooltip]
-  (when-not (string? tooltip-text)
-    (let [actor (Tooltip/.getTarget tooltip)
-          ; acturs might be initialized without a stage yet so we do when-let
-          ; FIXME double when-let
-          ctx (when-let [stage (actor/get-stage actor)]
-                (stage/get-ctx stage))]
-      (when ctx ; ctx is only set later for update!/draw! ... not at starting of initialisation
-        (Tooltip/.setText tooltip (str (tooltip-text ctx)))))))
+(defn- update-fn [tooltip-text]
+  (fn [tooltip]
+    (when-not (string? tooltip-text)
+      (let [actor (Tooltip/.getTarget tooltip)
+            ; acturs might be initialized without a stage yet so we do when-let
+            ; FIXME double when-let
+            ctx (when-let [stage (actor/get-stage actor)]
+                  (stage/get-ctx stage))]
+        (when ctx ; ctx is only set later for update!/draw! ... not at starting of initialisation
+          (Tooltip/.setText tooltip (str (tooltip-text ctx))))))))
 
 (defn add!
   [actor tooltip-text]
   (let [text? (string? tooltip-text)
         label (doto (label/create (if text? tooltip-text ""))
-                (.setAlignment (align/k->value :center)))]
+                (.setAlignment (align/k->value :center)))
+        update-text! (update-fn tooltip-text)]
     (doto (proxy [Tooltip] []
             ; hooking into getWidth because at
             ; https://github.com/kotcrab/vis-blob/master/ui/src/main/java/com/kotcrab/vis/ui/widget/Tooltip.java#L271
             ; when tooltip position gets calculated we setText (which calls pack) before that
             ; so that the size is correct for the newly calculated text.
             (getWidth []
-              (update-fn this)
+              (update-text! this)
               (proxy-super getWidth)))
       (.setTarget  actor)
       (.setContent label)))
