9db74ad4739a037522200691d21838c46ef2a745
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 19:44:34 +0530

    Move image to widget

--- src/clojure/vis_ui/widget.clj
+++ src/clojure/vis_ui/widget.clj
@@ -6,10 +6,16 @@
             [clojure.gdx.scenes.scene2d.ui.table :as table]
             [clojure.gdx.scenes.scene2d.utils :as utils]
             [clojure.vis-ui.tooltip :as tooltip])
-  (:import (com.badlogic.gdx.scenes.scene2d.ui Button)
+  (:import (com.badlogic.gdx.graphics Texture)
+           (com.badlogic.gdx.graphics.g2d TextureRegion)
+           (com.badlogic.gdx.scenes.scene2d.ui Button
+                                               Image)
            (com.badlogic.gdx.scenes.scene2d.utils ChangeListener
                                                   Drawable)
+           (com.badlogic.gdx.utils Align
+                                   Scaling)
            (com.kotcrab.vis.ui.widget VisCheckBox
+                                      VisImage
                                       VisImageButton
                                       VisLabel
                                       VisSelectBox
@@ -115,3 +121,29 @@
     (when center?          (.centerWindow   window))
     (when close-on-escape? (.closeOnEscape  window))
     (table/set-opts! window opts)))
+
+(defmulti ^:private image* type)
+
+(defmethod image* Drawable [^Drawable drawable]
+  (VisImage. drawable))
+
+(defmethod image* Texture [texture]
+  (VisImage. (TextureRegion. ^Texture texture)))
+
+(defmethod image* TextureRegion [texture-region]
+  (VisImage. ^TextureRegion texture-region))
+
+(defn image
+  [object
+   {:keys [scaling
+           align
+           fill-parent?]
+    :as opts}]
+  (let [^Image image (image* object)]
+    (when (= :center align)
+      (.setAlign image Align/center))
+    (when (= :fill scaling)
+      (.setScaling image Scaling/fill))
+    (when fill-parent?
+      (.setFillParent image true))
+    (widget/set-opts! image opts)))
